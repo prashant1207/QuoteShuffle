35688089671771023168cdd25ca8cde38244b622
Prashant Tiwari <tiwarip@microsoft.com>
2020-10-06 20:08:14 +0200

    Added Swipe Utility

--- QuoteShuffle/Model/Quote.swift
+++ QuoteShuffle/Model/Quote.swift
@@ -6,37 +6,7 @@
 //
 
 import Foundation
-import SwiftUI
 
 struct Quote: Codable {
     let quote, author: String
-}
-
-
-enum Swipe {
-    case up
-    case down
-    case left
-    case right
-    case unknown
-
-    enum Threshold {
-        static let horizontal: CGFloat = 30
-        static let vertical: CGFloat = 100
-    }
-
-    static func swipeType(_ translation: CGSize) -> Swipe {
-        var result: Swipe = .unknown
-        if translation.width < 0 && translation.height > -Swipe.Threshold.horizontal && translation.height < Swipe.Threshold.horizontal {
-            result = .left
-        } else if translation.width > 0 && translation.height > -Swipe.Threshold.horizontal && translation.height < Swipe.Threshold.horizontal {
-            result = .right
-        } else if translation.height < 0 && translation.width < Swipe.Threshold.vertical && translation.width > -Swipe.Threshold.vertical {
-            result = .up
-        } else if translation.height > 0 && translation.width < Swipe.Threshold.vertical && translation.width > -Swipe.Threshold.vertical {
-            result = .down
-        }
-
-        return result
-    }
 }
---
+++ QuoteShuffle/Utility/Swipe.swift
@@ -0,0 +1,37 @@
+//
+//  Swipe.swift
+//  QuoteShuffle
+//
+//  Created by Prashant Tiwari on 06/10/20.
+//
+
+import Foundation
+import SwiftUI
+
+enum Swipe {
+    case up
+    case down
+    case left
+    case right
+    case unknown
+
+    enum Threshold {
+        static let horizontal: CGFloat = 30
+        static let vertical: CGFloat = 100
+    }
+
+    static func swipeType(_ translation: CGSize) -> Swipe {
+        var result: Swipe = .unknown
+        if translation.width < 0 && translation.height > -Swipe.Threshold.horizontal && translation.height < Swipe.Threshold.horizontal {
+            result = .left
+        } else if translation.width > 0 && translation.height > -Swipe.Threshold.horizontal && translation.height < Swipe.Threshold.horizontal {
+            result = .right
+        } else if translation.height < 0 && translation.width < Swipe.Threshold.vertical && translation.width > -Swipe.Threshold.vertical {
+            result = .up
+        } else if translation.height > 0 && translation.width < Swipe.Threshold.vertical && translation.width > -Swipe.Threshold.vertical {
+            result = .down
+        }
+
+        return result
+    }
+}
